022c1447db793b6f10502f222caf679427d704c9
wangsij <wangsijiaaa@gmail.com>
2024-04-24 04:19:46 -0400

    Enemy movement updated
    Enemies can move in a pattern. When hitting a wall, they can go down and move to the opposite direction.

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    //the move speed of the enemy ships
+    public float moveSpeed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,18 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //movement of enemy ships
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
     }
+
+    //when enemies hit the boundary, they go to the opposite horizontal direction
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Boundary") {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
+
+            moveSpeed *= -1;
+        }
+    }
+
 }
